57465db5f03af590f772a1a02ecbdc0b62bc4cbc
Kassidy <kqssidy@gmail.com>
2021-10-22 10:28:17 -0500

    Initial Commit

---
+++ Three Letter Word Hackwich/Three_Letter_Word_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_HackwichApp.swift
+//  Three Letter Word Hackwich
+//
+//  Created by KSerek on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
